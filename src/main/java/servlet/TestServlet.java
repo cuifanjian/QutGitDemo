518007e58f9ee03e906830344d38218c20abed37
CuiFanJian <3062797565@qq.com>
2020-03-11 15:56:44 +0800

    lecture materials update

---
+++ src/main/java/servlet/TestServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import dao.TestDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ApplicationContext appCon = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //通过容器获取test实例
+        TestDao tt = (TestDao)appCon.getBean("test");
+        tt.sayHello();
+        response.getWriter().println("Test has been done!");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
