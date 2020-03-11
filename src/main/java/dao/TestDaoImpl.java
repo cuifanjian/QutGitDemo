e9e9485d4ab45ad4529b20ce6fd8b24a0ae499fe
CuiFanJian <3062797565@qq.com>
2020-03-11 16:14:33 +0800

    第一次测试

--- src/main/java/dao/TestDaoImpl.java
+++ src/main/java/dao/TestDaoImpl.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class TestDaoImpl implements TestDao{
 	@Override
 	public void sayHello() {
-		System.out.println("Hello, 崔繁坚！");
+		System.out.println("Hello, 崔繁坚！！！");
 	}
 }
